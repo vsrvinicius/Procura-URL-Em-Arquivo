218a8c843efb992fc3241b2427c5795cc2574881
Vinicius <107567877+vsrvinicius@users.noreply.github.com>
2022-10-27 15:58:31 -0300

    original

---
+++ teste.py
@@ -0,0 +1,20 @@
+import socket
+from typing import Type
+
+host = "google.com"
+count = 0
+
+with open("lista.txt", "r") as f:
+    lista = [line.strip() for line in f if line.strip()]
+    lista.sort()
+    for url in lista:
+        if ".com" in url:
+            count += 1
+            try:
+                with open("resultado.txt","a") as arquivo:
+                    arquivo.write(url+": " + socket.gethostbyname(url) + "\n")
+                    #print(url+": " + socket.gethostbyname(url))
+            except socket.gaierror:
+                pass
+
+print("Total de {} urls encontradas!!".format(count))
